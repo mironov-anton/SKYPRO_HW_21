9dbf49bdcc2f6f5cd1c918a16da4a8d54e9dd67e
mironov-anton <miron.khuzd@gmail.com>
2022-02-21 12:58:30 +0300

    Request class ready, main ready

--- logistic_classes.py
+++ logistic_classes.py
@@ -31,17 +31,24 @@ class Store(Storage):
     def add(cls, title, qnt):
         if cls._get_free_space() >= qnt:
             cls.__items[title] = cls.__items.get(title, 0) + qnt
+            return True
         else:
             print("На складе недостаточно места, попробуйте что-то другое")
+            return False
 
     @classmethod
     def remove(cls, title, qnt):
         if cls.__items[title] > qnt:
+            print("Нужное количество есть на складе")
             cls.__items[title] -= qnt
+            return True
         elif cls.__items[title] == qnt:
+            print("Нужное количество есть на складе")
             cls.__items.pop(title, None)
+            return True
         else:
             print("Не хватает на складе, попробуйте заказать меньше")
+            return False
 
     @classmethod
     def _get_free_space(cls):
@@ -50,6 +57,12 @@ class Store(Storage):
     @classmethod
     def _get_items(cls):
         return cls.__items
+
+    @classmethod
+    def print_items(cls):
+        print("На складе хранится:\n")
+        [print(value, key) for key, value in cls._get_items().items()]
+        return ""
 
     @classmethod
     def _get_unique_items_count(cls):
@@ -64,17 +77,24 @@ class Shop(Storage):
     def add(cls, title, qnt):
         if cls._get_free_space() >= qnt and cls._get_unique_items_count() < 5:
             cls.__items[title] = cls.__items.get(title, 0) + qnt
+            return True
         else:
             print("В магазине недостаточно места, попробуйте что-то другое")
+            return False
 
     @classmethod
     def remove(cls, title, qnt):
         if cls.__items[title] > qnt:
+            print("Нужное количество есть в магазине")
             cls.__items[title] -= qnt
+            return True
         elif cls.__items[title] == qnt:
+            print("Нужное количество есть в магазине")
             cls.__items.pop(title, None)
+            return True
         else:
             print("Не хватает в магазине, попробуйте заказать меньше")
+            return False
 
     @classmethod
     def _get_free_space(cls):
@@ -85,5 +105,27 @@ class Shop(Storage):
         return cls.__items
 
     @classmethod
+    def print_items(cls):
+        print("В магазине хранится:\n")
+        [print(value, key) for key, value in cls._get_items().items()]
+        return ""
+
+    @classmethod
     def _get_unique_items_count(cls):
         return len(cls.__items.keys())
+
+
+class Request:
+    def __init__(self, str_request):
+        lst = self._get_info(str_request)
+        self.from_ = lst[4]
+        self.to = lst[6]
+        self.amount = int(lst[1])
+        self.product = lst[2]
+
+    @staticmethod
+    def _get_info(str_request):
+        return str_request.split(" ")
+
+    def __repr__(self):
+        return f'Доставить {self.amount} {self.product} из {self.from_} в {self.to}'
